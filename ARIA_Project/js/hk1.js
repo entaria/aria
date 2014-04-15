60d8cec4b373375f2d32ddbe6002c0d7a2b61cd1
jhugh <jhugh@github.com>
2014-04-15 13:25:00 +0100

    qr code testing commit 2

---
+++ ARIA_Project/js/hk1.js
@@ -0,0 +1,39 @@
+var QRcode = (function() {
+    // Private stuff up here
+
+    // Public methods here
+    return {
+       // method 1
+       generateQRcode: function(options) {  
+            $('#qrcode').qrcode(options);
+       },
+    
+       // method 2
+       method2: function(operand,num) {     //operand = number to be subtracted, num = number subtracted from
+            var op = parseFloat(operand);
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                return num - op;
+       },
+       
+       // method 3
+       method3: function(operand,num) {    //operand = number to be multiplied by, num = number to be multiplied
+            var op = parseFloat(operand);
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                return op * num;
+       },
+       
+       // method 4
+       method4: function(operand,num) {     //operand = number to divide by, num = number to be divided
+            var op = parseFloat(operand);
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                return num/op;
+       }
+        
+    }
+}());
