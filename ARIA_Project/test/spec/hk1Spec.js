90d653bb54198826bddc3c7741a7942837baf3f9
jhugh <jhugh@github.com>
2014-04-15 15:30:26 +0100

    add email address test

--- ARIA_Project/test/spec/hk1Spec.js
+++ ARIA_Project/test/spec/hk1Spec.js
@@ -1,31 +1,36 @@
 describe("Aria Routines", function() {
     //basic operation tests
     it("generate QRcode", function () {
-        expect(QRcode.generateQRcode("great expectations")).toBe("?");
+        //alert("table " + jsUtils.generateQRtable("great expectations").createCanvas());
+        expect(jsUtils.generateQRtable("great expectations")).toBe("?");
     })
     /*
     it("generate QRcode", function () {
-        expect(QRcode.generateChecksum(11111111111111)).toBe(?);
-    })
-    it("check email address ", function () {
-        expect(Validation.emailAddressCheck("hugh.kelly@student.ncirl.ie")).toBe(true);
-    })
-    it("check email address ", function () {
-        expect(Validation.emailAddressCheck("hugh.kelly@student")).toBe(false);
-    })
-    it("check email address ", function () {
-        expect(Validation.emailAddressCheck("@student.ncirl.ie")).toBe(false);
-    })
-    it("validate geocode ", function () {
-        expect(Validation.geocodeCheck("55 0 0W 0 0 0E")).toBe(true);
-    })
-    it("validate geocode ", function () {
-        expect(Validation.geocodeCheck("55 0 61W 0 0 0E")).toBe(false);
-    })
-    it("validate geocode ", function () {
-        expect(Validation.geocodeCheck("55 0 61W 0 0 0E")).toBe(false);
+        expect(jsUtils.generateChecksum(11111111111111)).toBe("?");
     })
     */
+    it("check email address 1 ", function () {
+        expect(jsUtils.validateEmail("hugh.kelly@student.ncirl.ie")).toBe(true);
+    })
+    it("check email address 2 ", function () {
+        expect(jsUtils.validateEmail("hugh.kelly@student")).toBe(false);
+    })
+    it("check email address 3 ", function () {
+        expect(jsUtils.validateEmail("hugh.kelly.student.ncirl.ie")).toBe(false);
+    })
+    it("check email address 4 ", function () {
+        expect(jsUtils.validateEmail("@student.ncirl.ie")).toBe(false);
+    })
+    it("validate geocode 1 ", function () {
+        expect(Validation.geocodeCheck("55 0 0W 0 0 0E")).toBe(true);
+    })
+    it("validate geocode 2 ", function () {
+        expect(Validation.geocodeCheck("55 0 61W 0 0 0E")).toBe(false);
+    })
+    it("validate geocode 3 ", function () {
+        expect(Validation.geocodeCheck("55 0 61W 0 0 0E")).toBe(false);
+    })
+    
     
     //handle non-numerics
     /*
