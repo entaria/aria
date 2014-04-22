9df8fd86c398c4bb7fded92882c7c9e498794d2d
akabob <mcswebtech2014@gmail.com>
2014-04-22 22:20:23 +0100

    API Call working

--- ARIA_Project/js/CheckBalances.js
+++ ARIA_Project/js/CheckBalances.js
@@ -4,14 +4,28 @@
     // through the services array and genreate a li
     // element for every one of its items.
 
+/*angular.module('myModule')
+    .config(function ( $httpProvider) {        
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }).factory('featuresData', function ($http) {
+        return{          
+            doCrossDomainGet: function() {
+                return $http({
+                    url: 'http://entariabob.azurewebsites.net/api/ccb/GetClientCardBalances',
+                    method: 'GET'
+                })
+            }        
+        }
+}); */
 
-   
+
+
        $http.get('http://entariabob.azurewebsites.net/api/ccb/GetClientCardBalances').then(function (result) {
   //  $.get('http://entariaBOB.azurewebsites.net/api/ccb/GetClientCardBalances').then(function (result) {
        //    http://entariabob.azurewebsites.net/api/ccb/GetClientCardBalances
         $scope.ClientCardBalances = result.data;
 
-       
+        alert($scope.ClientCardBalances[1].PointsBalance + " " + $scope.ClientCardBalances[1].CardId);
     }
        )
 
