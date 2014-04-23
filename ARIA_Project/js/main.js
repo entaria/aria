52c807bea4b0c9a083b46634e2890998cd7962b8
jhugh <jhugh@github.com>
2014-04-23 12:26:55 +0100

    Merged Adrian/Brendan/Hugh code

--- ARIA_Project/js/main.js
+++ ARIA_Project/js/main.js
@@ -3,41 +3,35 @@ var Jsroutines = (function() {
 
     // Public methods here
     return {
-       // method 1
-       method1: function(operand,num) {   //operand = number to be added, num = number added to
-            var op = parseFloat(operand);
-                if (isNaN(op) == true) {
-                    throw new Error ("Not a number");
-                }
-                return op + num;
-       },
+
+        // Generate QR Code
+        generateQRcode: function(options) {  
+            $('#qrcode').qrcode(options);
+        },
     
-       // method 2
-       method2: function(operand,num) {     //operand = number to be subtracted, num = number subtracted from
+        generateQRtable: function(options) {  
+            $('#qrcode').qrcode(options);
+        },
+
+        validateEmail: function(emailAddress) {
+            var re = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            if (re.test(emailAddress)) {
+                return true;
+            } 
+            else {
+                return false;
+            }
+        },
+
+        // method 2
+        method2: function(operand,num) {     //operand = number to be subtracted, num = number subtracted from
             var op = parseFloat(operand);
                 if (isNaN(op) == true) {
                     throw new Error ("Not a number");
                 }
                 return num - op;
-       },
-       
-       // method 3
-       method3: function(operand,num) {    //operand = number to be multiplied by, num = number to be multiplied
-            var op = parseFloat(operand);
-                if (isNaN(op) == true) {
-                    throw new Error ("Not a number");
-                }
-                return op * num;
-       },
-       
-       // method 4
-       method4: function(operand,num) {     //operand = number to divide by, num = number to be divided
-            var op = parseFloat(operand);
-                if (isNaN(op) == true) {
-                    throw new Error ("Not a number");
-                }
-                return num/op;
-       }
-        
+        }
+
+    
     }
 }());
