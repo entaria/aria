37abc06a4d1e3dac24c510be48676e26f68884fa
adrianmann <adrianmatthiasmann@hotmail.com>
2014-04-29 20:40:07 +0100

    Added Brendan's rotating cube to the index and altered the css and images

---
+++ ARIA_Project/js/postdata.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function () {
+
+
+  //  cardholderdetails = [{"CardHolderDetailsId" : 0, "FirstName": "Gerry", "LastName": "McConnell", "EmailAddress": "Gerry@email123.com", "PhoneNo": "08276543000" }];
+ /*   cardholderdetails = [];
+    cardholderdetails.push(
+        {"CardHolderDetailsId" : null, "FirstName": "Gerry", "LastName": "McConnell", "EmailAddress": "Gerry@email123.com", "PhoneNo": "08276543000" }
+        );*/
+
+   
+    $("#postbutton1").click(function () {
+        $.post('http://entariaBOB.azurewebsites.net/api/chd/apipostCHD/{"FirstName": "Gerry", "LastName": "McConnell", "EmailAddress": "Gerry@email123.com", "PhoneNo": "08276543000"}' + cardholderdetails, function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+
+
+    $("#postbutton").click(function () {
+        
+
+        var fn = $('#firstname').val();
+        var ln = $('#surname').val();
+        var em = $('#email').val();
+        var ph = $('#telephone').val();
+        alert("Fiestadsdsd " + fn);
+        cardholderdetails = [];
+        cardholderdetails.push(
+            { CardHolderDetailsId:0, FirstName: fn, LastName: ln, EmailAddress: em, PhoneNo: ph }
+            );
+
+   //     alert("Posting " + cardholderdetails[0].FirstName + "  " + cardholderdetails[0].LastName + "  " + cardholderdetails[0].EmailAddress + "  " + cardholderdetails[0].PhoneNo);
+        var ok = null;
+        $.ajax({
+
+
+        type: "POST",
+        contentType: "application/json",
+        url: 'http://entariaBOB.azurewebsites.net/api/chd/apipostCHD',
+        data: JSON.stringify(cardholderdetails[0]),
+        success: ok,
+        dataType: "JSON"
+    });
+ //   alert("after Posting " + cardholderdetails[0].FirstName + "  " + cardholderdetails[0].LastName + "  " + cardholderdetails[0].EmailAddress + "  " + cardholderdetails[0].PhoneNo);
+
+    });
+});
---
+++ ARIA_Project/test/spec/adrian_spec.js
@@ -0,0 +1,5 @@
+describe("AngularJs Slider", function () {
+    it("should navigate through a list of images", function () {
+        expect(4 + 2).toEqual(6);
+    })
+})
