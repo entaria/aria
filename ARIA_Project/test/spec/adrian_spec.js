796aaddebdfc95712dbc983012b157dcd99b01f4
adrianmann <adrianmatthiasmann@hotmail.com>
2014-04-30 12:18:16 +0100

    First jasmine testing attempt

--- ARIA_Project/test/spec/adrian_spec.js
+++ ARIA_Project/test/spec/adrian_spec.js
@@ -1,5 +1,25 @@
-describe("AngularJs Slider", function () {
-    it("should navigate through a list of images", function () {
-        expect(4 + 2).toEqual(6);
-    })
-})
+describe('NavCtrl', function() {
+    var $scope, $location, $rootScope, createController;
+
+    beforeEach(inject(function($injector) {
+        $location = $injector.get('$location');
+        $rootScope = $injector.get('$rootScope');
+        $scope = $rootScope.$new();
+
+        var $controller = $injector.get('$controller');
+
+        createController = function() {
+            return $controller('NavCtrl', {
+                '$scope': $scope
+            });
+        };
+    }));
+
+    it('should have a method to check if the path is active', function() {
+        var controller = createController();
+        $location.path('/about');
+        expect($location.path()).toBe('/about');
+        expect($scope.isActive('/about')).toBe(true);
+        expect($scope.isActive('/contact')).toBe(false);
+    });
+});
